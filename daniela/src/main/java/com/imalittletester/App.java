3c64b35fd04bbbc0992064f5855bcbc9ec11bdec
tescar <daniela.tescar@gmail.com>
2022-09-29 18:24:27 +0300

    test

---
+++ daniela/src/main/java/com/imalittletester/App.java
@@ -0,0 +1,13 @@
+package com.imalittletester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
