6296cd9605adc1627fa7e7f7be43e2bd1156b12f
Jaradat <miriam.jaradat@gmail.com>
2022-10-06 18:32:33 +0300

    ApricotJamTest class declare

---
+++ Miriam2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
@@ -0,0 +1,45 @@
+package com.imalittletester.jam;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.io.File;
+import java.sql.Date;
+import java.sql.SQLOutput;
+
+public class ApricotJamTest {
+
+    public String sweetener;
+    public float sweetenerQty;
+    public String sweetenerUom;
+
+    public float apricotQty;
+    public String apricotUom;
+
+    public int jarQty;
+    public int jarCapacity;
+
+    public boolean isDietetic;
+
+}
+
+
+
+//    public int aPositiveInt = 10;
+//    public long aPositiveLong = 1000000000;
+//    public float aFloat = 1.5F;
+//    public double aDouble = 1.5;
+//    public boolean aBoolean;
+//    public String aSweetener = "lalaalllallaaaa\njahsdjvb";
+//    public File newFile;
+//    public Date newDate;
+//
+//    @Test
+//    void firstTest () {
+//        System.out.println(aPositiveInt);
+//        System.out.println(aPositiveLong);
+//        System.out.println(aFloat);
+//        System.out.println(aDouble);
+//        System.out.println(aBoolean);
+//        System.out.println(aSweetener);
+}
