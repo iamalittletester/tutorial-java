14b08b793e75c5152b7034cced2ba69f4ee3fc44
geoldessofia <sofiageoldes@yahoo.com>
2022-10-06 18:58:16 +0300

    New Module and added dependencies.

---
+++ Sofia2/src/main/java/com/imalittletester/jam/ApricotJam.java
@@ -0,0 +1,17 @@
+package com.imalittletester.jam;
+
+public class ApricotJam {
+
+    public String sweetner;
+    public float sweetnerQty;
+    public String sweetnerUom;
+
+    public float apricotQty;
+    public String apricotUom;
+
+    public int jarQty;
+    public int jarCapacity;
+
+    public boolean isDietetic;
+
+}
---
+++ Sofia2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
@@ -0,0 +1,27 @@
+package com.imalittletester.jam;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class ApricotJamTest {
+    public int aPositiveInt = 10;
+    public long aPositiveLong = -10000000;
+    public float aFloat = 500;
+    public double aDouble = -1.5697777777777700000000000000000000000000000000005668;
+    public boolean aBoolean;
+    public String aSweetner = "sugar\\cane";
+
+    @Test
+    void firstTest() {
+ //      System.out.println(aPositiveInt);
+  //      System.out.println(aPositiveLong);
+  //      System.out.println(aFloat);
+  //      System.out.println(aDouble);
+   //     System.out.println(aBoolean);
+        System.out.println(" --> " + aSweetner);
+
+
+
+    }
+}
