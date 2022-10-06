0e5a61ab7e5a879158db641b6b5ba29afa0dbbd2
popescuangela <angela.popescu@garmin.com>
2022-10-06 18:31:31 +0300

    declared ApricotJam fields.

---
+++ angela2/src/main/java/com/imalittletester/jam/ApricotJam.java
@@ -0,0 +1,13 @@
+package com.imalittletester.jam;
+
+public class ApricotJam {
+    public String sweetener;
+    public float sweetenerQty;
+    public String sweetenerUom;
+
+    public float apricotQty;
+    public String apricotUom;
+    public int jarQty;
+    public int jarCapacity;
+    public boolean isDietetic;
+}
