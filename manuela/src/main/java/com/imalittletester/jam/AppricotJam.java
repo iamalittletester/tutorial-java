cdd01afe5b273e277b8be25e11b62239cf1deb01
porfiremanuel <manuela.pop@garmin.com>
2022-10-06 18:35:09 +0300

    commit AppricotJamTest class

---
+++ manuela/src/main/java/com/imalittletester/jam/AppricotJam.java
@@ -0,0 +1,6 @@
+package com.imalittletester.jam;
+
+public class AppricotJam {
+    //declaration of a field
+       public int nameOfInt = 3;
+}
---
+++ manuela/src/test/java/com/imalittletester/jam/AppricotJamTest.java
@@ -0,0 +1,26 @@
+package com.imalittletester.jam;
+
+import org.junit.jupiter.api.Test;
+
+public class AppricotJamTest {
+//fields
+    public String sweetener;
+    public float sweetenerQty;
+    public String sweetenerUom;
+
+    public float apricotQty;
+    public String apricotUom;
+
+    public int jarQty;
+    public int jarCapacity;
+
+    public boolean isDietetic;
+//method
+
+    @Test
+    void firstTest() {
+        //sout+tab for print
+
+    }
+
+}
