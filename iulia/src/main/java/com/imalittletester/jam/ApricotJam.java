1f3bada3693b819b439103e780cac5e460ab4068
lukacsiulia <iullia.lukacs@garmin.com>
2022-10-06 18:30:42 +0300

    created branch, and imported common lang 3 and common io

---
+++ iulia/src/main/java/com/imalittletester/jam/ApricotJam.java
@@ -0,0 +1,24 @@
+package com.imalittletester.jam;
+
+public class ApricotJam {
+
+    //fields - to describe state (ex proprietatile unui obiect)
+    //       - trebuie sa aiba visibility attribute + type (primitiv sau obiect) + si trebuie sa ii dam nume
+    //       - fieldul va fi definit in toate metodele care apartin clasei
+    // primitive data types - int/ long/ byte/ short etc (numeric integral)
+
+    public String sweetener;
+    public float sweetenerQty;
+    public String sweetenerUom;
+
+    public float apricotQty;
+    public String apricotUom;
+
+    public int jarQty;
+    public int jarCapacity;
+
+    public boolean isDietetic;
+
+
+    //method
+}
---
+++ iulia/src/test/java/com/imalittletester/jam/ApricotJamTest.java
@@ -0,0 +1,28 @@
+package com.imalittletester.jam;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Date;
+
+public class ApricotJamTest {
+
+    public int aPositiveValue = 10;
+    public long aPositiveLong = -1001001;
+    public float aFloat = 500;
+    public double aDouble = 29292.29292929229;
+    public boolean aBoolean;
+    public String aSweetener = "sugar\ncane";
+    public File newFile;
+    public Date newDate;
+
+    @Test
+    void firstTest() {
+       // System.out.println(aPositiveValue);
+       // System.out.println(aPositiveLong);
+       // System.out.println(aFloat);
+       // System.out.println(aDouble);
+       // System.out.println(aBoolean);
+        System.out.println(" -->" + aSweetener);
+    }
+}
