5064aedf32781cc810d45ab3b361e7aca5ed448c
iamalittletester <capreoara6@gmail.com>
2022-10-06 18:34:28 +0300

    Session 1 : packages, classes, primitive types.

---
+++ corina2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
@@ -0,0 +1,26 @@
+package com.imalittletester.jam;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Date;
+
+public class ApricotJamTest {
+    public int aPositiveInt = -10;
+    public long aPositiveLong = -100000000;
+    public float aFloat = 500;
+    public double aDouble = -221211.556765746464564564564564645645632342435465564;
+    public boolean aBoolean;
+    public String aSweetener = "sugar cane";
+    public Date new_Date;
+
+    @Test
+    void firstTest() {
+//        System.out.println(aPositiveInt);
+//        System.out.println(aPositiveLong);
+//        System.out.println(aFloat);
+//        System.out.println(aDouble);
+//        System.out.println(aBoolean);
+        System.out.println(" --> " + aSweetener);
+    }
+}
