0c315239b76d9674cb2165dcea18ea50b72d1ffe
lukacsiulia <iullia.lukacs@garmin.com>
2022-10-10 15:02:17 +0300

    defined garmin cookies

---
+++ iulia/src/main/java/com/imalittletester/garmincookies/NID.java
@@ -0,0 +1,15 @@
+package com.imalittletester.garmincookies;
+
+public class NID {
+    public String name = "NID";
+    public String domain = ".google.com";
+    public char path = '/';
+    public int size = 178;
+    public String sameSite = "None";
+    public boolean sameParty = false;
+    public String partitionKey;
+
+    {
+        partitionKey = null;
+    }
+}
---
+++ iulia/src/main/java/com/imalittletester/garmincookies/uuid2.java
@@ -0,0 +1,17 @@
+package com.imalittletester.garmincookies;
+
+import javax.print.attribute.DateTimeSyntax;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class uuid2 {
+
+    public String name = "uuid2";
+    public int size = 24;
+    public long value = 1071970293196698480L;
+    public boolean httpOnly = true;
+    public char secure = '✓';
+    public String priority = "Medium";
+    LocalDateTime date = LocalDateTime.parse("2023-01-08T06:38:10.919Z");
+
+}
