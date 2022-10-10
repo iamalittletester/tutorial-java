3e620072cb9d27a794bb38c2398c29025d8def1b
berindepaula <paula.berinde@garmin.com>
2022-10-10 12:53:27 +0300

    Commit GarminCookie class.

---
+++ paula/src/main/java/com/imalittletester/cookies/GarminCookie.java
@@ -0,0 +1,13 @@
+package com.imalittletester.cookies;
+
+public class GarminCookie {
+
+    public String name;
+    public String value;
+    public String domain;
+    public String priority;
+    public int size;
+    public boolean httpOnly;
+    public boolean secure;
+
+}
---
+++ paula/src/test/java/com/imalittletester/cookies/GarminCookieTest.java
@@ -0,0 +1,5 @@
+package com.imalittletester.cookies;
+
+public class GarminCookieTest {
+
+}
