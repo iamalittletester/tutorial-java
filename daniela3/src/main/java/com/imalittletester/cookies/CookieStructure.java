ba9de6b3433b53b8c97d00d07491c9403c712c52
tescar <daniela.tescar@gmail.com>
2022-10-11 23:59:33 +0300

    homework cookies

--- daniela3/src/main/java/com/imalittletester/cookies/CookieStructure.java
+++ daniela3/src/main/java/com/imalittletester/cookies/CookieStructure.java
@@ -2,13 +2,11 @@ package com.imalittletester.cookies;
 
 public class CookieStructure {
 
-   public String Name;
-   public String Value;
-   public String Domain;
-   public String Path;
-
-
-   public int Size;
-   public boolean HttpOnly;
+   public String name;
+   public String value;
+   public String domain;
+   public String path;
+   public int size;
+   public boolean httpOnly;
 
 }
