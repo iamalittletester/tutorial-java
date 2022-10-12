0cc773994a6e81178695d0edb28aa9b78686020d
geoldessofia <sofiageoldes@yahoo.com>
2022-10-12 16:22:16 +0300

    GarminCookie class is added.

---
+++ Sofia2/src/main/java/com/imalittletester/cookies/GarminCookie.java
@@ -0,0 +1,14 @@
+package com.imalittletester.cookies;
+
+public class GarminCookie {
+
+    public String name;
+    public String value;
+    public String domain;
+    public int size;
+    public boolean httpOnly;
+    public String expires;
+    public boolean secure;
+    public String priority;
+    public String sameSite;
+}
