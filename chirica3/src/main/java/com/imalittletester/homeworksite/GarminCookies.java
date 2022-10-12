a7f0445956af187dc504182688b70574ed6dbccc
LexyChi <alexandra.chirica@garmin.com>
2022-10-12 22:03:06 +0300

    homework for class no 2 - Oct 6th

---
+++ chirica3/src/main/java/com/imalittletester/homeworksite/GarminCookies.java
@@ -0,0 +1,12 @@
+package com.imalittletester.homeworksite;
+
+import java.util.Date;
+
+public class GarminCookies {
+    public String cookieName;
+    public String cookieValue;
+    public Date cookieExpiration;
+    public boolean httpOnly;
+    public boolean secure;
+    public int cookieSize;
+}
