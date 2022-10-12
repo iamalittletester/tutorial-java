a255fdec639952138834b1c306b5ed170285ead3
Jaradat <miriam.jaradat@gmail.com>
2022-10-12 23:30:02 +0300

    ApricotJam changes and corrections

--- Miriam2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
+++ Miriam2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
@@ -9,37 +9,24 @@ import java.sql.SQLOutput;
 
 public class ApricotJamTest {
 
-    public String sweetener;
-    public float sweetenerQty;
-    public String sweetenerUom;
-
-    public float apricotQty;
-    public String apricotUom;
-
-    public int jarQty;
-    public int jarCapacity;
-
-    public boolean isDietetic;
-
-}
+    public int aPositiveInt = 10;
+    public long aPositiveLong = 1000000000;
+    public float aFloat = 500;
+    public double aDouble = 1.5;
+    public boolean aBoolean;
+    public String aSweetener = "sugar cane";
+    public File newFile;
+    public Date newDate;
 
 
+   @Test
+   void firstTest () {
 
-//    public int aPositiveInt = 10;
-//    public long aPositiveLong = 1000000000;
-//    public float aFloat = 1.5F;
-//    public double aDouble = 1.5;
-//    public boolean aBoolean;
-//    public String aSweetener = "lalaalllallaaaa\njahsdjvb";
-//    public File newFile;
-//    public Date newDate;
-//
-//    @Test
-//    void firstTest () {
 //        System.out.println(aPositiveInt);
 //        System.out.println(aPositiveLong);
 //        System.out.println(aFloat);
 //        System.out.println(aDouble);
 //        System.out.println(aBoolean);
 //        System.out.println(aSweetener);
+    }
 }
