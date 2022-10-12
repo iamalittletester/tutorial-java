313feba06f8a37a2e2b72232379c509d84175bb3
el-amouramira <amour2343@gmail.com>
2022-10-12 16:22:01 +0300

    GarminCookie class added

---
+++ amira2/src/main/java/com/imalittletester/cookies/GarminCookie.java
@@ -0,0 +1,13 @@
+package com.imalittletester.cookies;
+
+public class GarminCookie {
+
+     String name;
+     String value;
+     String domain;
+     int size;
+     boolean httpOnly;
+     String expires;
+     boolean secure;
+     String priority;
+}
