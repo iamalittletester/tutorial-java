7a4c157ef6c7473f06a3046daaa53b58c450317d
Jaradat <miriam.jaradat@gmail.com>
2022-10-12 23:50:33 +0300

    Homework - Garmin Cookies

---
+++ Miriam2/src/main/java/com/imalittletester/Cookies/CookieJar.java
@@ -0,0 +1,12 @@
+package com.imalittletester.Cookies;
+
+public class CookieJar {
+    //fields
+    public String name;
+    public String value;
+    public String domain;
+    public int size;
+    public boolean httpOnly;
+    public boolean secure;
+    public String priority;
+}
