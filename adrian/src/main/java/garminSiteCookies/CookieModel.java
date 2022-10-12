537bf3341da05d458ba64e1c6e511a632924ddcf
avram-vasile-adrian <adrian.avram@garmin.com>
2022-10-12 23:02:11 +0300

    homework cookies

---
+++ adrian/src/main/java/garminSiteCookies/CookieModel.java
@@ -0,0 +1,13 @@
+package garminSiteCookies;
+
+
+import java.util.Date;
+
+public class CookieModel {
+
+    public String name = "notice_poptime";
+    public String value = "c1:1%7Cc2:1%7Cc3:1%7Cc4:1%7Cc5:1%7Cc6:1%7Cc7:1%7Cc8:1%7Cc9:1%7Cc10:1%7Cc11:1%7Cc12:1%7Cc13:1%7Cc14:1%7Cc15:1%7Cts:1665601926578%7Cconsent:true";
+    public String domain = "garmin.com";
+    public int size = 43;
+    public boolean httpOnly;
+}
