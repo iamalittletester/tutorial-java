dbdad2c6cebc4e1d6067be604be2ac66b8670c1b
popescuangela <angela.popescu@garmin.com>
2022-10-12 22:10:51 +0300

    declared garmin Cookies structure

---
+++ angela2/src/main/java/com/imalittletester/garmincookies/GarminCookies.java
@@ -0,0 +1,16 @@
+package com.imalittletester.garmincookies;
+
+public class GarminCookies {
+    public String name;
+    public String value;
+    public String domain;
+    public String path;
+    public String maxAge;
+    public int size;
+    public boolean isHttpOnly;
+    public boolean isSecure;
+    public String sameSite;
+    public boolean isSameParty;
+    public String partitionKey;
+
+}
