793cf3106a88c283c49d2780db30deeb27bf0685
bencovicivlad <vlad.bencovici@gmail.com>
2022-10-17 16:48:10 +0300

    curs 2

--- vladbengo/src/main/java/com/imalittletester/jam/AppricotJam.java
+++ vladbengo/src/main/java/com/imalittletester/jam/AppricotJam.java
@@ -1,6 +1,8 @@
 
 package com.imalittletester.jam;
 
+import com.imalittletester.App;
+
 public class AppricotJam {
     //fields
     public String sweetener;
@@ -8,11 +10,56 @@ public class AppricotJam {
     public String sweetenerUom;
 
 
-    public float appricotQty;
-    public String appricotUom;
+    public float apricotQty;
+    public String apricotUom;
 
     public int jarQty;
     public int jarCapacity;
 
     public boolean isDietetic;
+
+    //method
+    public AppricotJam() {}
+
+    public AppricotJam(String sweetener) {
+        this.sweetener = sweetener;
+    }
+
+    public AppricotJam(String sweetener, float sweetenerQty, String sweetenerUom, float apricotQty, String apricotUom,
+                      int jarQty, int jarCapacity, boolean isDietetic) {
+        this.sweetener = sweetener;
+        this.sweetenerQty = sweetenerQty;
+        this.sweetenerUom = sweetenerUom;
+        this.apricotQty = apricotQty;
+        this.apricotUom = apricotUom;
+        this.jarQty = jarQty;
+        this.jarCapacity = jarCapacity;
+        this.isDietetic = isDietetic;
+    }
+
+    public void makeJam() {
+        System.out.println("Adding " + sweetenerQty + " " + sweetenerUom + " of " + sweetener);
+        System.out.println("Also adding: " + apricotQty + " " + apricotUom + " of apricots");
+        System.out.println("Boiling for " + preparationTime() + " minutes");
+        System.out.println("Is dietetic? " + isDietetic);
+    }
+
+    private int preparationTime() {
+        return 30;
+    }
+
+    @Override
+    public String toString() {
+        return "ApricotJam{" +
+                "sweetener='" + sweetener + '\'' +
+                ", sweetenerQty=" + sweetenerQty +
+                ", sweetenerUom='" + sweetenerUom + '\'' +
+                ", apricotQty=" + apricotQty +
+                ", apricotUom='" + apricotUom + '\'' +
+                ", jarQty=" + jarQty +
+                ", jarCapacity=" + jarCapacity +
+                ", isDietetic=" + isDietetic +
+                '}';
+    }
+
 }
--- vladbengo/src/test/java/com/imalittletester/jam/AppricotJamTest.java
+++ vladbengo/src/test/java/com/imalittletester/jam/AppricotJamTest.java
@@ -2,6 +2,9 @@ package com.imalittletester.jam;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.Date;
+
 public class AppricotJamTest {
 
     public int aPositiveInt = 10;
@@ -10,14 +13,32 @@ public class AppricotJamTest {
     public double aDouble = -221211.55676574647;
     public boolean aBoolean;
     public String aSweetener = "Sugar cane";
+    public Date new_Date;
+
+    public AppricotJam apricotJam = new AppricotJam();
+    public AppricotJam apricotJam2 = new AppricotJam("white sugar");
+    public AppricotJam apricotJam3 = new AppricotJam("sugar cane", 1.5f, "kg", 10, "kg", 30, 500, false);
+
 
     @Test
+    void secondTest() {
+        System.out.println(apricotJam);
+
+        System.out.println(apricotJam2);
+
+        System.out.println(apricotJam3);
+        apricotJam3.makeJam();
+        System.out.println(apricotJam3.sweetener);
+
+        apricotJam3.sweetener.contains("sugar");
+    }
+    @Test
     void firstTest() {
-        System.out.println(aPositiveInt);
-        System.out.println(aPositiveLong);
-        System.out.println(aFloat);
-        System.out.println(aDouble);
-        System.out.println(aBoolean);
-        System.out.println(aSweetener);
+//        System.out.println(aPositiveInt);
+//        System.out.println(aPositiveLong);
+//        System.out.println(aFloat);
+//        System.out.println(aDouble);
+//        System.out.println(aBoolean);
+        System.out.println(" --> " + aSweetener);
     }
 }
