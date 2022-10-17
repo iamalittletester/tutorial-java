1ba26eed04eb97da184735f2dae114f5e21f6140
Jaradat <miriam.jaradat@gmail.com>
2022-10-17 22:06:43 +0300

    Homework - constructors and methods

---
+++ Miriam2/src/test/java/com/imalittletester/cookies/CookieJarTest.java
@@ -0,0 +1,17 @@
+package com.imalittletester.cookies;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class CookieJarTest {
+
+    public CookieJar cookieJar1 = new CookieJar("SAPISID", "_q9P1hrCVJgedAEY/AHLr9hh8WoDOJwKYt");
+    public CookieJar cookieJar2 = new CookieJar("SSID","ABT8AEeWIUUJ1H464", ".google.ro", 21, true, true, "High");
+
+    @Test
+    void cookieTest () {
+        System.out.println(cookieJar1);
+        System.out.println(cookieJar2);
+    }
+}
