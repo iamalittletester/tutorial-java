4809aabbe776f8cdaaebe57b82699aeda1416b6a
lukacsiulia <iullia.lukacs@garmin.com>
2022-10-17 15:01:17 +0300

    cookie constructors and methods

--- iulia/src/main/java/com/imalittletester/garmincookies/CookieStructure.java
+++ iulia/src/main/java/com/imalittletester/garmincookies/CookieStructure.java
@@ -11,5 +11,47 @@ public class CookieStructure {
     public char secure = '✓';
     public String priority;
     public Date expirationDate;
+    private boolean isProvidedParamSubDomainOfDomain;
 
-}
+    public CookieStructure () { //constructor default
+    }
+
+    public CookieStructure(String name, int size, long value, boolean httpOnly, char secure, String priority, Date expirationDate, boolean isProvidedParamSubDomainOfDomain) {
+        this.name = name;
+        this.size = size;
+        this.value = value;
+        this.httpOnly = httpOnly;
+        this.secure = secure;
+        this.priority = priority;
+        this.expirationDate = expirationDate;
+        this.isProvidedParamSubDomainOfDomain = isProvidedParamSubDomainOfDomain;
+    }//constructor with all fields
+
+
+    public CookieStructure(String name, long value) {
+        this.name = name;
+        this.value = value; //constructor name and value
+    }
+
+    @Override
+    public String toString() {
+        return "CookieStructure{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", value=" + value +
+                ", httpOnly=" + httpOnly +
+                ", secure=" + secure +
+                ", priority='" + priority + '\'' +
+                ", expirationDate=" + expirationDate +
+                ", isProvidedParamSubDomainOfDomain=" + isProvidedParamSubDomainOfDomain +
+                '}';
+    }
+
+
+    public boolean isProvidedParamSubDomainOfDomain() {
+        return isProvidedParamSubDomainOfDomain;
+    }
+
+
+    }
+
