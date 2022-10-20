2aa4180f386c3af6cceb29d077b98795b28270fb
iamalittletester <capreoara6@gmail.com>
2022-10-20 18:36:16 +0300

    Chapter 3: inheritance

--- corina2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
+++ corina2/src/test/java/com/imalittletester/jam/ApricotJamTest.java
@@ -1,10 +1,14 @@
 package com.imalittletester.jam;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.File;
 import java.util.Date;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ApricotJamTest {
     public int aPositiveInt = -10;
     public long aPositiveLong = -100000000;
@@ -16,8 +20,22 @@ public class ApricotJamTest {
 
     public ApricotJam apricotJam = new ApricotJam();
     public ApricotJam apricotJam2 = new ApricotJam("white sugar");
-    public ApricotJam apricotJam3 = new ApricotJam("sugar cane", 1.5f, "kg", 10, "kg", 30, 500, false);
+    public ApricotJam apricotJam3 = new ApricotJam("sugar cane", 1.5f, "kg", 10, "kg", 30, 500, true);
 
+    public MelonJam melonJam = new MelonJam("plain sugar", 10, "kg", 10, "kg", new Bottle(20, 1000), false);
+
+    @Order(1)
+    @Test
+    void thirdTest() {
+        apricotJam3.makeJam();
+        System.out.println(apricotJam3.getSweetener());
+        apricotJam3.setSweetener("stevia");
+        System.out.println(apricotJam3.getSweetener());
+        System.out.println("---------------------");
+        melonJam.makeJam();
+    }
+
+    @Order(2)
     @Test
     void secondTest() {
         System.out.println(apricotJam);
@@ -29,8 +47,11 @@ public class ApricotJamTest {
         System.out.println(apricotJam3.sweetener);
 
         apricotJam3.sweetener.contains("sugar");
+
+        melonJam.makeJam();
     }
 
+    @Order(3)
     @Test
     void firstTest() {
 //        System.out.println(aPositiveInt);
