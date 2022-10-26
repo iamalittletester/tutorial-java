680938bc0d133c4392ee4b5120374e65d9964a76
hapliuca <anamaria.hapliuc@garmin.com>
2022-10-26 22:44:32 +0300

    commit tema1

---
+++ HapliucA/src/main/java/com/imalittletester/cookie/CookieFields.java
@@ -0,0 +1,13 @@
+package com.imalittletester.cookie;
+
+import java.util.Date;
+
+public class CookieFields {
+    public String name;
+    public String value;
+    public String domain;
+    public Date expires;
+    public int size;
+    public boolean secure;
+
+}
