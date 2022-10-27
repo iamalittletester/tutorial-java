b6e6370670b1c14043d5b65289d6453a36b5bb0b
spalnacan <bianca.spalnacan@garmin.com>
2022-10-27 11:39:56 +0300

    Commit new module and cookie class

---
+++ BiancaS/src/main/java/com/imalittletester/cookie/Cookie.java
@@ -0,0 +1,19 @@
+package com.imalittletester.cookie;
+
+public class Cookie {
+        //Proprieties
+        public String name;
+        public String value;
+        public String domain;
+        public String path;
+        public String expires;
+        public int  size;
+        public boolean httpOnly;
+        public boolean secure;
+        public String sameSite;
+        public boolean sameParty;
+        public String partitionKey;
+        public String priority;
+        //Constructor
+
+}
