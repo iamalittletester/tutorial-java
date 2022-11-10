58d3d7c35505d53108af30537f57d3056b974c2f
spalnacanbogd <bogdan.spalnacan@hotmail.com>
2022-11-10 18:14:09 +0200

    lecture 7

--- bogdanul/src/test/java/com/imalittletester/jam/OperatorsTest.java
+++ bogdanul/src/test/java/com/imalittletester/jam/OperatorsTest.java
@@ -3,7 +3,77 @@ package com.imalittletester.jam;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 public class OperatorsTest {
+
+
+    @Test
+    void collectionsTest() {
+        List<String> lista1 = new ArrayList<>();
+        System.out.println(lista1.isEmpty());
+        lista1.add("Cluj");
+        lista1.add("Oradea");
+        System.out.println("lista1 = " + lista1);
+
+        List<String> lista2 = Arrays.asList("peach", "apple", "melon");
+        System.out.println("lista2 = " + lista2);
+        System.out.println("Lista2 contains apple: " + lista2.contains("apple"));
+
+        List<Integer> lista3 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        System.out.println("lista3 = " + lista3);
+        System.out.println(lista3.get(5));
+        System.out.println(lista3);
+
+        List<Integer> listaDeNumere = List.of(5, 13, 25, 8, 14, 7, 9);
+        System.out.println(listaDeNumere.size());
+        int numberOfNumbers = 0, number2 = 0;
+
+        List<Integer> positionInList = new ArrayList<>();
+
+        for (int i = 0; i < listaDeNumere.size(); i++) {
+            if (listaDeNumere.get(i) % 7 == 0)
+                numberOfNumbers++;
+            positionInList.add(i);
+        }
+        System.out.println("numberOfNumbers = " + numberOfNumbers);
+        System.out.println("Elements on the position: " + positionInList);
+
+        for (int numarInLista : listaDeNumere) {
+            if (numarInLista % 7 == 0)
+                number2++;
+        }
+        System.out.println("Number 2...: " + number2);
+    }
+
+
+    @Test
+    void mapTest() {
+        Map<String, Integer> simpleMap = new HashMap<>();
+        simpleMap.put("first", 1);
+        System.out.println("simpleMap = " + simpleMap);
+
+        Map<String, Integer> anotherMap = Map.of("firstKey", 1, "secondKey", 2, "thirdKey", 3);
+        System.out.println("anotherMap = " + anotherMap);
+        System.out.println("Is key present? " + anotherMap.containsKey("secondaryKey"));
+        System.out.println("Is value present? " + anotherMap.containsValue(3));
+        System.out.println("The value of 'secondKey' is: " + anotherMap.get("secondKey"));
+
+        System.out.println("Te map size is: " + anotherMap.size());
+        System.out.println("Is the map empty? " + anotherMap.isEmpty());
+
+        for (Map.Entry<String, Integer> entry : anotherMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + " - value: " + entry.getValue());
+        }
+        for (String key : anotherMap.keySet()) {
+            System.out.println(key);
+        }
+
+        for (int value : anotherMap.values()) {
+            System.out.println(value);
+        }
+
+    }
 
     @Test
     void firstTest() {
@@ -38,7 +108,7 @@ public class OperatorsTest {
         System.out.println(!bool1);
         System.out.println(!bool2);
         boolean e = !"eeee".contains("e");
-        System.out.println("String equals: " +string1.equals(string2));
+        System.out.println("String equals: " + string1.equals(string2));
     }
 
     @Test
@@ -55,7 +125,7 @@ public class OperatorsTest {
 
         boolean int1MaiMareSauNu = int1 > cuiAsignamValoarea;
         System.out.println("Int1 mai mare ca celalalt? " + (int1 > cuiAsignamValoarea));
-        System.out.println("Int1 mai mic ca celalalt? " + (int1 <cuiAsignamValoarea));
+        System.out.println("Int1 mai mic ca celalalt? " + (int1 < cuiAsignamValoarea));
     }
 
     @Test
